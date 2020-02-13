262fbbe593055798c57d6d6174b478b3032919a2
Dorian Guillaume <drnguillaume@gmail.com>
2020-02-13 09:29:36 +0100

    Add solution/gitignore/bdd

---
+++ bdd/requetes.sql
@@ -0,0 +1,5 @@
+/*RESTORE BACKUP*/
+/* restore database GrandHotel FROM DISK = N'C:\Users\Shadow\workspace\projetAPI-GTM\bdd\GrandHotel.bak'
+with
+move 'GrandHotel' to  N'C:\Users\Shadow\workspace\projetAPI-GTM\bdd\GrandHotel.mdf',
+move 'GrandHotel_log' to  N'C:\Users\Shadow\workspace\projetAPI-GTM\bdd\GrandHotel.ldf' */
